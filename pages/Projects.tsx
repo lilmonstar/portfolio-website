1063ffe6c8ca7b9e3993ad6058cea01280c8b27d
michael <michael@example.com>
2023-06-05 18:18:57 +0800

    27 commit

--- pages/Projects.tsx
+++ pages/Projects.tsx
@@ -6,9 +6,9 @@ function Projects() {
     <div className='max-w-[1024px] min-h-screen mx-auto p-2 flex flex-col items-center justify-evenly' id='projects'>
       <h2 className='mt-8 mb-4'>My Projects</h2>
       <div className='grid md:grid-cols-3 gap-4 md:gap-6 lg:gap-8'>
-        <div className='col-span-1 mx-1 bg-fuchsia-500 rounded-lg shadow-md shadow-gray-300 w-[250px] h-[300px]'></div>
-        <div className='col-span-1 mx-1 bg-fuchsia-500 rounded-lg shadow-md shadow-gray-300 w-[250px] h-[300px]'></div>
-        <div className='col-span-1 mx-1 bg-fuchsia-500 rounded-lg shadow-md shadow-gray-300 w-[250px] h-[300px]'></div>
+        <div className='col-span-1 mx-1 bg-gradient-to-br from-fuchsia-50 to-fuchsia-800 rounded-lg shadow-md shadow-gray-500 w-[250px] h-[300px]'></div>
+        <div className='col-span-1 mx-1 bg-gradient-to-br from-fuchsia-50 to-fuchsia-800 rounded-lg shadow-md shadow-gray-500 w-[250px] h-[300px]'></div>
+        <div className='col-span-1 mx-1 bg-gradient-to-br from-fuchsia-50 to-fuchsia-800 rounded-lg shadow-md shadow-gray-500 w-[250px] h-[300px]'></div>
       </div>
     </div>
   )
