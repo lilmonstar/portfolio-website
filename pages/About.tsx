2e12d25c41da68d059dd16b8d9f8866f84c98b70
michael <michael@example.com>
2023-06-08 10:10:35 +0800

    39 commit

--- pages/About.tsx
+++ pages/About.tsx
@@ -11,7 +11,7 @@ function About() {
         <Image src={profileImg} alt='profile' className='object-cover mx-4 max-w-[200px] h-[250px]'></Image>
         <div className='max-w-xs ms:max-w-md lg:max-w-lg mx-4 flex flex-col gap-1 sm:gap-2 md:gap-3 lg:gap-4'>
           <h4 className='text-xl text-slate-700 dark:text-slate-100 font-mono font-bold'>Who I Am</h4>
-          <p className='text-gray-600 dark:text-gray-100'>Work in progress... Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore cupiditate quod laborum dolor magni vero quisquam exercitationem explicabo consequatur expedita illum reprehenderit, a eum. </p>
+          <p className='text-gray-600 dark:text-gray-100'>Hi, my name is <span className='text-sky-400'>Michael Balderas.</span> I&apos;m a tech enthusiast with more than six years of experience in programming spanning from developing websites for all platforms, programming microcontroller devices, and programming in automation, machine learning and artificial intelligence with Python. I like playing games, watching movies and learning new things. </p>
         </div>
       </div>
     </div>
--- pages/Services.tsx
+++ pages/Services.tsx
@@ -10,20 +10,20 @@ function Services() {
     <div className='max-w-[1024px] min-h-screen mx-auto p-2 flex flex-col items-center justify-evenly' id='services'>
       <h2 className='mt-10 mb-8'>My Services</h2>
       <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8'>
-        <div className='col-span-1 mx-1 flex flex-col items-center justify-center text-white gap-4 bg-[#262626] rounded-lg shadow-md shadow-gray-500 w-[300px] h-[300px] hover:-translate-y-4 hover:bg-[#ff004f] ease-in duration-500'>
+        <div className='col-span-1 mx-1 flex flex-col items-center justify-center text-white gap-4 bg-[#262626] rounded-lg shadow-md shadow-gray-500 w-[300px] h-[340px] hover:-translate-y-4 hover:bg-[#ff004f] ease-in duration-500'>
           <FaCode size={40}/>
           <h3 className='font-bold'>Web Design</h3>
-          <p className='max-w-[70%]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, consectetur.</p>
+          <p className='max-w-[70%]'>With more than five years of experience of building websites. Making use of modern ways to build websites.</p>
         </div>
-        <div className='col-span-1 mx-1 flex flex-col items-center justify-center text-white gap-4 bg-[#262626] rounded-lg shadow-md shadow-gray-500 w-[300px] h-[300px] hover:-translate-y-4 hover:bg-[#ff004f] ease-in duration-500'>
+        <div className='col-span-1 mx-1 flex flex-col items-center justify-center text-white gap-4 bg-[#262626] rounded-lg shadow-md shadow-gray-500 w-[300px] h-[340px] hover:-translate-y-4 hover:bg-[#ff004f] ease-in duration-500'>
           <VscSmiley size={40}/>
           <h3 className='font-bold'>UI/UX Design</h3>
-          <p className='max-w-[70%]'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, officia.</p>
+          <p className='max-w-[70%]'>I can easily render any project design or copy the UI of currently working websites on the web. I do responsive design so the app looks good on all platforms.</p>
         </div>
-        <div className='col-span-1 mx-1 flex flex-col items-center justify-center text-white gap-4 bg-[#262626] rounded-lg shadow-md shadow-gray-500 w-[300px] h-[300px] hover:-translate-y-4 hover:bg-[#ff004f] ease-in duration-500'>
+        <div className='col-span-1 mx-1 flex flex-col items-center justify-center text-white gap-4 bg-[#262626] rounded-lg shadow-md shadow-gray-500 w-[300px] h-[340px] hover:-translate-y-4 hover:bg-[#ff004f] ease-in duration-500'>
           <TbSettingsAutomation size={40}/>
           <h3 className='font-bold'>Automation</h3>
-          <p className='max-w-[70%]'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam, a numquam harum rem soluta molestiae optio.</p>
+          <p className='max-w-[70%]'>I can do automation programming both software and hardware to ease manual task. I can integrate machine learning and artificial intelligence to my code to automate certain tasks.</p>
         </div>
       </div>
     </div>
